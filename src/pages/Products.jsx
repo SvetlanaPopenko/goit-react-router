de95fdc7d47863896c4555ea7c83ebbde36bb3ce
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-01 01:32:05 +0200

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,25 @@
 import { ProductList } from "components/ProductList";
 import { getProducts } from "FakeAPI";
+import { useSearchParams } from "react-router-dom";
+import { SearchBox } from "components/SearchBox";
+
 
 export const Products = () => {
-    const products = getProducts();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const productName = searchParams.get("name")??"";
+        const products = getProducts();
+
+    const visibleProducts = products.filter((product) => product.name.toLocaleLowerCase().includes(productName.toLocaleLowerCase()));
+
+    const updateQueryString = (name) => {
+        const nextParams = name !== "" ? { name } : {};
+        setSearchParams(nextParams);
+    };
 
     return (
         <main>
-            <ProductList products={products}/>
+            <SearchBox value={productName} onChange={ updateQueryString} />
+                  <ProductList products={visibleProducts}/>
         </main>
     );
 }
