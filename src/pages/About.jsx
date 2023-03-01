eb95c9275371cb51bd6f4974cc9a8d4fb203a291
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-01 21:00:58 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
-import { Link, Outlet } from "react-router-dom";
+import { Suspense } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
-export const About = () => {
+const About = () => {
   return (
     <div>
       <h1>About us</h1>
@@ -11,19 +12,23 @@ export const About = () => {
         possimus assumenda non exercitationem sed architecto consectetur
         officia, a soluta consequatur? Adipisci reiciendis necessitatibus,
         voluptatum officia corporis recusandae suscipit hic natus!
-          </p>
-          <ul>
-              <li>
-                  <Link to="mission">Read about our mission</Link>
-              </li>
-              <li>
-                  <Link to="team">Get to know the team</Link>
-              </li>
-              <li>
-                  <Link to="reviews">Go through the reviews</Link>
-              </li>
-          </ul>
-          <Outlet/>
+      </p>
+      <ul>
+        <li>
+          <Link to="mission">Read about our mission</Link>
+        </li>
+        <li>
+          <Link to="team">Get to know the team</Link>
+        </li>
+        <li>
+          <Link to="reviews">Go through the reviews</Link>
+        </li>
+      </ul>
+      <Suspense fallback={<div>Loading subpage...</div>}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
+
+export default About;
